e6e5fb9f5da8d8730ffcd4a1de620485a14b3e2b
ChidakashSugandhi <99040053+ChidakashSugandhi@users.noreply.github.com>
2023-04-04 15:52:35 +0530

    Uploading the project

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GlobalStyles } from './global-styles';
+import { App } from './app';
+import { FirebaseContext } from './context/firebase';
+
+const config = {
+  apiKey: '',
+  authDomain: 'netflix-c8ae9.firebaseapp.com',
+  databaseURL: 'https://netflix-c8ae9.firebaseio.com',
+  projectId: 'netflix-c8ae9',
+  storageBucket: 'netflix-c8ae9.appspot.com',
+  messagingSenderId: '590575805931',
+  appId: '',
+};
+
+const firebase = window.firebase.initializeApp(config);
+
+ReactDOM.render(
+    <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+        <GlobalStyles />
+        <App />
+    </FirebaseContext.Provider>, 
+    document.getElementById('root')
+);
